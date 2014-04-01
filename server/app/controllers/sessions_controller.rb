62c66d6d29619fb0a74662dd00d9e4ea0181fb1b
Sebastian Osuna <sebastian.osuna@skaphe.com>
2014-03-31 23:02:04 -0500

    Dependency added: rails_config. See #config/settings.yml to set the global password.

--- server/app/controllers/sessions_controller.rb
+++ server/app/controllers/sessions_controller.rb
@@ -1,11 +1,14 @@
 class SessionsController < ApplicationController
-
 	def requestSession
 		if params[:username] and params[:password]
-			render json: { username: params[:username], password: params[:password] }
-		else
-			render json: { error: 'Unauthorized', status: '401' }, :status => '401'
+			@password = params[:password]
+			if @password == Settings.global.password
+				render json: { username: params[:username], password: @password }
+				return
+			end
 		end
+		# Send 401 
+		render json: { error: 'Unauthorized', status: '401' }, :status => '401'
 	end
 
 end
